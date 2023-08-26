63ee8213d03a7b1b371f50306bf9ddf45e0cf1b1
bishal-rumba007 <bishal.rumba007@gmail.com>
2023-08-26 15:00:35 +0545

    fix leakage

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
+require('dotenv').config()
 
-mongoose.connect("mongodb+srv://lite1bishal:warduser@cluster0.l1uajrq.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(`${mongoURI}`).then(()=>{
     console.log("connected!");
 }).catch((err)=>{
     console.log("error");
